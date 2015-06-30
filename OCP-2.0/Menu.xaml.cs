de9b48e1cf907e678e373cc64609bbe58ed82665
ep2012 <eli-price@uiowa.edu>
2015-06-30 07:16:42 -0500

    Moved Stuff Over

---
+++ OCP-2.0/Menu.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OCP_2._0
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            lblDate.Content = DateTime.Today.ToLongDateString();
+            lblUsername.Content = "Logged in as: " + Environment.UserName;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
